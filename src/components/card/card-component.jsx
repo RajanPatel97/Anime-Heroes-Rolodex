0f3a227700875cef936ebbf8125cd52c3aa4faaa
Rajan Patel <rajan.n.patel.97@gmail.com>
2021-02-01 19:12:28 +0000

    updated card styling

--- src/components/card/card-component.jsx
+++ src/components/card/card-component.jsx
@@ -3,6 +3,6 @@ import './card-component.styles.css'
 export const Card = ({hero}) => (
     <div className='card-container'>
         <img alt= "hero" src={`${hero.img}`}/>
-         <h1> {hero.name}</h1>
+        <h1>{hero.name}</h1>
     </div>
 )
